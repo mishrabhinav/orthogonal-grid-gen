855141aecc77d4964b3e02c4b9018ee16f8b9639
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-07-27 15:46:09 -0700

    Works for perfect cuboids.

--- src/com/slb/Main.java
+++ src/com/slb/Main.java
@@ -1,5 +1,6 @@
 package com.slb;
 
+import com.slb.gridcomponents.Cell;
 import com.slb.gridcomponents.Grid;
 import com.slb.utils.GridUtils;
 
@@ -9,5 +10,17 @@ public class Main {
 
         GridUtils loader = new GridUtils(args[0]);
         Grid grid = loader.loadGrid();
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print(grid.getCells().get(i).toString());
+        }
+
+        System.out.println();
+
+        Cell test = loader.test1();
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print(grid.getCells().get(i).toString());
+        }
     }
 }
