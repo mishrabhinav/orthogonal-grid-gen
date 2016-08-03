cb0c9b2197c058d37def1fb1bb742aab6b31b1b3
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-08-02 19:03:23 -0700

    Add Globals

---
+++ src/com/slb/utils/Globals.java
@@ -0,0 +1,22 @@
+package com.slb.utils;
+
+/**
+ * Created by abhinavmishra on 02/08/2016.
+ */
+public final class Globals {
+
+    private Globals() {
+
+    }
+
+    public static final String GRID_NUMBEROFCELLS = "numberOfCells";
+    public static final String GRID_CELLS = "cells";
+    public static final String GRID_CELL_ID = "id";
+    public static final String GRID_CELL_CENTRE = "centre";
+    public static final String GRID_CELL_VERTICES = "vertices";
+    public static final String GRID_CELL_NUMBEROFVERTICES = "numberOfVertices";
+    public static final String GRID_CELL_NEIGHBOURS = "neighbours";
+    public static final String GRID_VERTEX_X = "x";
+    public static final String GRID_VERTEX_Y = "y";
+    public static final String GRID_VERTEX_Z = "z";
+}
