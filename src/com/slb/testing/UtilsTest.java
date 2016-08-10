e709df83b3b5d5da2ef86c175522b6b32ecd234f
Abhinav Mishra <AMishra12@slb.com>
2016-08-10 11:56:15 +0100

    Add face normal and vector angle test cases

--- src/com/slb/testing/UtilsTest.java
+++ src/com/slb/testing/UtilsTest.java
@@ -1,52 +1,88 @@
 package com.slb.testing;
 
+import com.slb.components.Cell;
+import com.slb.components.Vector;
+import com.slb.utils.Utils;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by abhinavmishra on 27/07/2016.
  */
 public class UtilsTest {
 
+    @Ignore
     @Test
     public void getBarycenter() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void getFaceCenter() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void valueWithPlane() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void arePointsOnSameSide() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void insideCell() throws Exception {
 
     }
 
+    @Ignore
     @Test
     public void vectorToCentre() throws Exception {
 
     }
 
     @Test
-    public void faceNormal() throws Exception {
+    public void faceNormalPlanar() throws Exception {
+        Utils utils = new Utils();
 
+        Cell c = new Cell(0, new Vector(0, 0, 0));
+        c.getVertices().add(0, new Vector(1,2,1) );
+        c.getVertices().add(1, new Vector(2,1,0) );
+        c.getVertices().add(2, new Vector(3,1,1) );
+        c.getVertices().add(3, new Vector(4,1,2) );
+
+        Vector v = utils.faceNormal(c, 0);
+
+        assertEquals(new Vector(2, 4, -2), v);
+    }
+
+    @Test
+    public void faceNormalNonPlanar() throws Exception {
+        Utils utils = new Utils();
+
+        Cell c = new Cell(0, new Vector(0, 0, 0));
+        c.getVertices().add(0, new Vector(1,2,1) );
+        c.getVertices().add(1, new Vector(2,1,0) );
+        c.getVertices().add(2, new Vector(4,1,1) );
+        c.getVertices().add(3, new Vector(4,1,2) );
+
+        Vector v = utils.faceNormal(c, 0);
+
+        assertEquals(new Vector(2, 6, -2), v);
     }
 
     @Test
     public void angleBetweenVectors() throws Exception {
-
+        Utils utils = new Utils();
+        assertEquals(Math.acos(1/Math.sqrt(2)), utils.angleBetweenVectors(new Vector(0,1,1), new Vector(0,1,0)), 0.00001);
     }
 
 }
